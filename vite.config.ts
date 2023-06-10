5822ed87d3daa1cd1a0c7d264c31c7b6b02f4737
Ray Foss <ray@vblip.com>
2023-06-10 16:39:54 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,12 @@ import pluginRewriteAll from 'vite-plugin-rewrite-all';
 import reactSWC from '@vitejs/plugin-react-swc';
 import react from '@vitejs/plugin-react';
 
-const isWebContainer = globalThis.process?.versions?.webcontainer;
-
-console.log(globalThis.process.env)
-
 export default defineConfig({
-  base: '/',
+  base: process?.env.VITE_BASE || '/',
   plugins: [
     pluginRewriteAll(),
     nodePolyfills(),
-    isWebContainer
+    process?.versions?.webcontainer
       ? react()
       : reactSWC(),
   ],
