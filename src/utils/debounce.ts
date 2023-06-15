bc92f6239f0f25be1a396ea4f622973e0a5838eb
Replit user <>
2023-06-15 04:26:45 +0000

    give TS more memory

--- src/utils/debounce.ts
+++ src/utils/debounce.ts
@@ -1,6 +1,6 @@
 // https://gist.github.com/ca0v/73a31f57b397606c9813472f7493a940
 export function debounce<T extends Function>(cb: T, wait = 150) {
-    let h = 0;
+    let h: NodeJS.Timeout;
     let callable = (...args: any) => {
         clearTimeout(h);
         h = setTimeout(() => cb(...args), wait);
