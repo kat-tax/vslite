a20ba9082822ec5ddc4969ba22f71ed3a4abf2af
FossPrime <ray@vblip.com>
2023-06-16 06:28:52 +0000

    trim vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
-import {defineConfig} from 'vite';
-import {nodePolyfills} from 'vite-plugin-node-polyfills';
+import { defineConfig } from 'vite';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import pluginRewriteAll from 'vite-plugin-rewrite-all';
 import reactSWC from '@vitejs/plugin-react-swc';
 import react from '@vitejs/plugin-react';
-
-
 
 export default defineConfig({
   base: globalThis.process?.env.VITE_BASE || '/',
@@ -26,6 +24,5 @@ export default defineConfig({
       'Cross-Origin-Embedder-Policy': 'require-corp',
       'Cross-Origin-Opener-Policy': 'same-origin',
     },
-    host: globalThis.process?.env.VITE_HOST || 'localhost'
   },
 });
